08a2ad678b8995da807f9128871d233cadcd2ce4
Jitendra Patidar <94981678+jitendra653@users.noreply.github.com>
2024-09-01 07:16:08 +0000

    remove axios import

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Pagination from './Pagination';
 import Card from './components/Card';
-import axios from 'axios';
 interface ApiData {
 key: [string]
 }
